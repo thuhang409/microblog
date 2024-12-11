d9282a8387af4f18c7d9479e61c351ca673eedee
thuhang409 <thuhang040900@gmail.com>
2024-12-11 17:57:15 +0700

    initial

---
+++ app/routes.py
@@ -0,0 +1,140 @@
+from datetime import datetime, timezone
+from flask import render_template, flash, redirect, url_for, request
+from urllib.parse import urlsplit
+from app import app
+import sqlalchemy as sa
+from app import db
+from flask_login import login_user, current_user, logout_user, login_required
+from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
+
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+
+
+@app.route("/")
+@app.route("/index")
+@login_required
+def index():
+    user = {"username":"amelia"}
+    posts = [{
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }]
+    return render_template('index.html', title='Home', posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        print(next_page)
+        print(urlsplit(next_page))
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+
+        # flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+        return redirect(next_page)
+    return render_template("login.html", title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a user.')
+        return redirect(url_for('login'))
+    return render_template('register.html', title = "Register", form=form)
+
+@app.route('/user/<username>')
+@login_required
+def profile(username):
+    user = db.first_or_404(sa.select(User).where(User.username==username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    form = EmptyForm()
+    return render_template('profile.html', title='Profile', user=user, posts=posts, form=form)
+
+@app.route('/edit-profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Changed successfully!")
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == username))
+        if user is None:
+            flash(f'User {username} not found.')
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('profile', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash(f'You are following {username}!')
+        return redirect(url_for('profile', username=username))
+    else:
+        return redirect(url_for('index'))
+    
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == username))
+        if user is None:
+            flash(f'User {username} not found.')
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash(f'You are not following {username}.')
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
