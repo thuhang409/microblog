82ddcf80e9891a00895c57170bcf469602476203
thuhang409 <thuhang040900@gmail.com>
2024-12-17 14:10:28 +0700

    unit testing improvements & fix env vars

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
-
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -11,4 +11,4 @@ class Config:
     
     POSTS_PER_PAGE = 3
 
-
+    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
